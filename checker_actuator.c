12f4eacd0418bff2bb28ff465b039ef0db4b9602
Samuel <samuelperleo@gmail.com>
2026-06-01 02:43:27 +0200

    Removed the print on the execution of the movements on the checker

--- checker_actuator.c
+++ checker_actuator.c
@@ -6,7 +6,7 @@
 /*   By: saperez- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 02:13:58 by saperez-          #+#    #+#             */
-/*   Updated: 2026/06/01 02:32:29 by saperez-         ###   ########.fr       */
+/*   Updated: 2026/06/01 02:42:26 by saperez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,35 @@
 void	ft_checker_actuator(t_list **a, t_list **b, char *input)
 {
 	if (ft_strncmp("sa\n", input, 4) == 0)
-		ft_swap_a(a);
+		ft_swap(a);
 	else if (ft_strncmp("sb\n", input, 4) == 0)
-		ft_swap_b(b);
+		ft_swap(b);
 	else if (ft_strncmp("ss\n", input, 4) == 0)
 	{
-		ft_swap_a(a);
-		ft_swap_b(b);
+		ft_swap(a);
+		ft_swap(b);
 	}
 	else if (ft_strncmp("pa\n", input, 4) == 0)
-		ft_push_a(a, b);
+		ft_push(a, b);
 	else if (ft_strncmp("pb\n", input, 4) == 0)
-		ft_push_b( a, b);
+		ft_push( b, a);
 	else if (ft_strncmp("ra\n", input, 4) == 0)
-		ft_rotate_a(a);
+		ft_rotate(a);
 	else if (ft_strncmp("rb\n", input, 4) == 0)
-		ft_rotate_b(b);
+		ft_rotate(b);
 	else if (ft_strncmp("rr\n", input, 4) == 0)
 	{
-		ft_rotate_a(a);
-		ft_rotate_b(b);
+		ft_rotate(a);
+		ft_rotate(b);
 	}
 	else if (ft_strncmp("rra\n", input, 5) == 0)
-		ft_reverse_rotate_a(a);
+		ft_reverse_rotate(a);
 	else if (ft_strncmp("rrb\n", input, 5) == 0)
-		ft_reverse_rotate_b(b);
+		ft_reverse_rotate(b);
 	else if (ft_strncmp("rrr\n", input, 5) == 0)
 	{
-		ft_reverse_rotate_a(a);
-		ft_reverse_rotate_b(b);
+		ft_reverse_rotate(a);
+		ft_reverse_rotate(b);
 	}
 	else
 	{
