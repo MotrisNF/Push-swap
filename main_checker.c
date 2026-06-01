31c3b34739b1bfb287f405946ce8d6fb84d7c0a9
Samuel <samuelperleo@gmail.com>
2026-06-01 03:11:33 +0200

    Added a prototipe from the checker. Right now, only need a parser to get the array of numbers and the size of the array. He can check the correct order of the stack a, and check if b its NULL. I think there is some bugs on this. Need to chech the correct use of the free b.

---
+++ main_checker.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_checker.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saperez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 02:47:17 by saperez-          #+#    #+#             */
+/*   Updated: 2026/06/01 03:07:48 by saperez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bonus.h"
+
+int	ft_check_order(t_list **stack, size_t size)
+{
+	size_t	i;
+	t_list	*tmp;
+
+	i = 1;
+	tmp = *stack;
+	while (tmp)
+	{
+		if (*(int *)tmp->content != i)
+			return (0);
+		tmp = (tmp)->next;
+		i++;
+	}
+	return (1);
+}
+
+
+int	main(int argc, char **argv)
+{
+	char	*line;
+	t_list	*stack_a;
+	t_list	*stack_b;
+	int	*numbers;
+	size_t	size;
+
+	numbers = ft_parser(argc, argv, &size);
+	if (!numbers)
+		return (1);
+	stack_a = ft_create_stack(numbers, size);
+	if (!stack_a)
+		return (1);
+	stack_b = NULL;
+	while ((line = get_next_line(0)))
+	{
+		ft_checker_actuator(&stack_a, &stack_b, line);
+		free(line);
+	}
+	if (ft_check_order(&stack_a, size) && stack_b == NULL)
+		ft_printf("OK\n");
+	else
+		ft_printf("KO\n");
+	return (0);
+}
+
+
